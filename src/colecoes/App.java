d5fce246d8dee6ffb240b11a19754aff37dc214b
DMouraLobo <diegomouralobo@outlook.com>
2025-01-20 19:12:52 -0300

    Entrega da atividade corrigida
    Corrigindo problemas no looping

--- src/colecoes/App.java
+++ src/colecoes/App.java
@@ -5,29 +5,33 @@ import java.util.ArrayList;
 public class App {
 
 	public static void main(String[] args) {
-		int i = 1;
+		int i= 1;
 		String confirm;
 		String resp;
 		String respArray[];
 		Scanner ler = new Scanner(System.in);
 		
 		ArrayList<String> listaM = new ArrayList<String>();
+		
 		ArrayList<String> listaF = new ArrayList<String>();
+		
 		while(i==1) {
-			System.out.println("Deseja fazer um registro: ");
-			confirm =ler.next();
+			System.out.println("Deseja fazer um registro? ");
+			confirm =ler.nextLine();
 			
 			if(confirm.equalsIgnoreCase("sim")) {
-				System.out.println("Digite o seu nome e sexo: (Separados por uma virgula EX: João , masculino)");
+				System.out.println("Digite o seu nome e sexo: (Separados por uma virgula EX: João,masculino)");
 				resp=ler.nextLine();
 				respArray =resp.split(",");
 				if(respArray.length >= 2) { 	
-					if (respArray[1].equalsIgnoreCase("masculino")) {
-					listaM.add(respArray[0]);
+					if (respArray[1].trim().equalsIgnoreCase("masculino")) {
+					listaM.add(respArray[0].trim());
 				
 					} else {
-					listaF.add(respArray[0]);
-					}		
+						
+					listaF.add(respArray[0].trim());
+					}
+					
 				}else {
 						System.out.println("Entrada inválida. Certifique-se em digitar o nome e o sexo separados por uma vírgula.");
 					}
@@ -39,6 +43,9 @@ public class App {
 				System.out.println(listaF);
 			}
 		}
+		
+		ler.close();
+		
 		}
 }
 	
